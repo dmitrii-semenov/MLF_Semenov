5b57e0c2f27d04d62904724afa6e69b5fe058722
evvejiji <240689@vutbr.cz>
2025-02-18 09:31:32 +0100

    Added

--- lecture2/task1.py
+++ lecture2/task1.py
@@ -1,5 +1,14 @@
-import matplotlib.pyplot as plt
-import copy
-import numpy as np
-from numpy.linalg import norm
-from sklearn.cluster import KMeans
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+iris=load_iris()
+iris.feature_names
+print(iris.feature_names)
+print(iris.data[0:5,:])
+print(iris.target[0:5])
+
+X=iris.data
+y=iris.target
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+print(X_train.shape)
+print(X_test.shape)
