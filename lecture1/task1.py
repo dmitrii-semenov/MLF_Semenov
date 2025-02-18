a794e6986c4c57fd7492625466487c8816106e74
evvejiji <240689@vutbr.cz>
2025-02-18 09:12:05 +0100

    Update task1.py

--- lecture1/task1.py
+++ lecture1/task1.py
@@ -3,8 +3,3 @@ import copy
 import numpy as np
 from numpy.linalg import norm
 from sklearn.cluster import KMeans
-
-loaded_points = np.load('Data/k_mean_points.npy')
-
-plt.figure()
-plt.scatter(loaded_points[:,0],loaded_points[:,1])
